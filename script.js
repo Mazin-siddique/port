6fa6e2266824971e8bfeb648d2721f68cf33187f
Mazin-siddique <106996274+Mazin-siddique@users.noreply.github.com>
2022-10-14 18:28:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 50){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scrollY > 500){
+
+            $('.scroll-up-btn').addClass("show");
+        } else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+        
+});
+$('.scroll-up-btn').click(function(){
+    $('html').animate({scrollTop: 0});
+});
+
+
+
+$('.menu-btn').click(function(){
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+});
+
+var typed =new Typed(".typing", {
+    strings: ["an Elecrical engineer","a developer" , "a Freelancer" ,"a Designer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop: true
+});  
+
+
+
+
+var typed =new Typed(".typing-2", {
+    strings: ["an Elecrical engineer","a developer" , "a Freelancer" ,"a Designer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop: true
+});  
+
+//owl carousel script
+ $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
